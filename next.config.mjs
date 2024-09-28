fd802b9a535346976c4aee9d707bbc4763f8dd3f
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-28 10:35:45 -0500

    feat: add the language switch

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
             {
                 source: '/sign-up',
                 destination: '/signup',
+            },
+            {
+                source: '/reset-password',
+                destination: '/resetPassword',
             }
         ];
     }
