23560c3a63f699b4a1e3d20be9b0be6dd69ae63f
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-30 00:51:48 -0500

    feat: add the backend connection

--- src/app/(account)/login/page.tsx
+++ src/app/(account)/login/page.tsx
@@ -1,57 +1,96 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
-import { Stack, Radio, RadioGroup } from "@chakra-ui/react";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import environment from "@/environments/enviroment";
 
 const Login = () => {
     const { t } = useTranslation("global");
-    const [value, setValue] = useState("staff");
-    const [username, setUsername] = useState("");
+    const [user, setUser] = useState({username: "", password: ""});
+    const router = useRouter();
 
-    const setRouterHandler = () => {
-        if (value) {
-            if (value === "staff") window.location.href = `/${value}/${username}/home`;
-            else if (value === "client") window.location.href = `/${value}/${username}/home`;
+    useEffect(() => {
+        setUser({
+            username: user.username,
+            password: user.password,
+        });
+    }, [user.username, user.password]);
+
+    const signInHandler = async () => {
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: user.username,
+                password: user.password,
+            }),
+        };
+
+        const response = await fetch(`${environment.serverBasePath}/authentication/sign-in`, requestOptions);
+        if (!response.ok) {
+            throw new Error("Login failed");
         }
+
+        const result = await response.json();
+        localStorage.setItem("token", result.token);
+        return result;
+    };
+
+    //if (localStorage.getItem("token")) {
+    //    const fetchData = async () => {
+    //        const data = await fetch(`${environment.serverBasePath}/users`);
+    //        if (data) {
+    //            if (data === "ROLE_CLIENT") router.push("/client/home");
+    //            else router.push("/staff/home");
+    //        }
+    //    }
+    //}
+
+    const notify = () => {
+        toast.promise(signInHandler(), {
+            loading: "Iniciando sesión",
+            success: "Sesión iniciada correctamente",
+            error: "Error: El usuario no está registrado 😔",
+        });
     };
 
     return (
-        <section className="flex items-center w-1/4 h-screen m-auto px-16">
+        <section className="flex items-center w-full sm:w-1/2 xl:w-1/5 h-screen m-auto">
+            <Toaster />
             <section className="w-full">
-                <h1 className="mt-8 text-4xl text-center font-extrabold tracking-tighter">{t("login.title")}</h1>
+                <h1 className="mt-8 text-5xl text-center font-extrabold tracking-tighter">
+                    {t("login.title")}
+                </h1>
                 <section className="flex flex-col mx-auto my-4">
                     <input
                         type="text"
                         placeholder={t("login.user-placeholder")}
                         className="c-input__input"
-                        onChange={(e) => { setUsername(e.target.value) }}
+                        onChange={(e) =>
+                            setUser({ ...user, username: e.target.value })
+                        }
                     />
                     <input
                         type="password"
                         placeholder={t("login.password-placeholder")}
                         className="c-input__input mt-2"
+                        onChange={(e) =>
+                            setUser({ ...user, password: e.target.value })
+                        }
                     />
-                    <RadioGroup
-                        onChange={setValue}
-                        value={value}
-                    >
-                        <Stack my={4}>
-                            <span>Loguearse como: </span>
-                            <section className="flex">
-                                <Radio value="staff" mr={4}>Staff mecánico</Radio>
-                                <Radio value="client">Cliente</Radio>
-                            </section>
-                        </Stack>
-                    </RadioGroup>
-                    <span className="my-2 text-center">
+                    <span className="my-4 text-center">
                         <Link href="/reset-password">
                             <u>{t("login.forgot-password")}</u>
                         </Link>
                     </span>
                     <button
                         className="c-button py-4 font-semibold"
-                        onClick={setRouterHandler}
+                        onClick={notify}
                     >
                         {t("login.button")}
                     </button>
--- src/app/(with-navbar)/staff/uploadItem/page.tsx
+++ src/app/(with-navbar)/staff/uploadItem/page.tsx
@@ -1,36 +1,88 @@
+"use client"
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+import environment from "@/environments/enviroment";
+
 const UploadItemPage = () => {
+    const [item, setItem] = useState({title: "", description: "", image: "", price: 0});
+
+    const uploadItemHandler = async () => {
+        const token = localStorage.getItem("token");
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                title: item.title,
+                description: item.description,
+                image: item.image,
+                price: item.price,
+            }),
+        };
+
+        try {
+            const response = await fetch(`${environment.serverBasePath}/products`, requestOptions);
+
+            if (!response.ok) throw new Error("Error registering item");
+
+            const result = await response.json();
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw new Error("Network error or server error");
+        }
+    };
+
+    const notify = () => {
+        toast.promise(
+            uploadItemHandler(), {
+                loading: "Subiendo item...",
+                success: "Item subido correctamente",
+                error: "Error: No se pudo subir el item 😔",
+            }
+        );
+    };
+
     return (
-        <article>
-            <section className="flex items-center w-1/4 mx-auto my-40 px-16">
-                <section className="w-full">
-                    <h1 className="mt-8 text-4xl text-center font-extrabold tracking-tighter">Upload item</h1>
-                    <section className="flex flex-col mx-auto my-4">
-                        <input
-                            type="text"
-                            placeholder="Enter the title order"
-                            className="c-input__input"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Enter the description"
-                            className="c-input__input mt-2"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Enter the image url"
-                            className="c-input__input mt-2"
-                        />
-                        <input
-                            type="number"
-                            placeholder="Enter the price"
-                            className="c-input__input mt-2"
-                        />
-                        <button
-                            className="c-button my-2 py-4 font-semibold"
-                        >
-                            Upload item
-                        </button>
-                    </section>
+        <article className="flex items-center w-full sm:w-1/2 xl:w-1/5 my-40 m-auto">
+            <Toaster />
+            <section className="w-full">
+                <h1 className="mt-8 text-4xl text-center font-extrabold tracking-tighter">Upload item</h1>
+                <section className="flex flex-col mx-auto my-4">
+                    <input
+                        type="text"
+                        placeholder="Enter the title order"
+                        className="c-input__input"
+                        onChange={(e) => setItem({...item, title: e.target.value})}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter the description"
+                        className="c-input__input mt-2"
+                        onChange={(e) => setItem({...item, description: e.target.value})}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter the image url"
+                        className="c-input__input mt-2"
+                        onChange={(e) => setItem({...item, image: e.target.value})}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Enter the price"
+                        className="c-input__input mt-2"
+                        onChange={(e) => setItem({...item, price: parseInt(e.target.value)})}
+                    />
+                    <button
+                        className="c-button my-2 py-4 font-semibold"
+                        onClick={notify}
+                    >
+                        Upload item
+                    </button>
                 </section>
             </section>
         </article>
